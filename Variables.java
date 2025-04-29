48ba3972b487da54ba55e389a01a769b3dc96b82
Keenan Joshua <joshua.onyango@strathmore.edu>
2025-04-29 13:20:05 +0300

    Challenge 2 - Using data type variables

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables{
+    public static void main(String[] args) {
+        double mean_marks;
+        double Math=85, English=69, Physics=77, Chemistry=82;
+        mean_marks=(Math+English+Physics+Chemistry)/4;
+        System.out.println("Maths: "+Math+" English: "+English+" Physics: "+Physics+" Chemistry: "+Chemistry);
+        System.out.println("The mean marks are "+mean_marks);
+    }
+}
